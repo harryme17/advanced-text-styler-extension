ccae0a73cc1773464dee9ef0434e3d1dcdefd0cd
harryme17 <meharshal1703@gmail.com>
2025-07-25 18:55:31 +0530

    Added Keyboard Shortcut ctrl+shift+s

--- background.js
+++ background.js
@@ -7,6 +7,7 @@ function setupContextMenu() {
         });
     });
 }
+
 chrome.runtime.onInstalled.addListener(setupContextMenu);
 chrome.runtime.onStartup.addListener(setupContextMenu);
 
@@ -19,6 +20,50 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
         });
     }
 });
+
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "open-style-text-modal") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            if (tabs.length > 0) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tabs[0].id },
+                    function: () => {
+                        const selectedText = window.getSelection().toString();
+                        if (selectedText) {
+                            const existingModal = document.getElementById('font-styler-modal-container');
+                            if (existingModal) { existingModal.remove(); }
+
+                            const modalContainer = document.createElement('div');
+                            modalContainer.id = 'font-styler-modal-container';
+                            modalContainer.onclick = () => modalContainer.remove();
+
+                            const iframe = document.createElement('iframe');
+                            iframe.id = 'font-styler-iframe';
+                            iframe.src = chrome.runtime.getURL(`popup.html?text=${encodeURIComponent(selectedText)}`);
+                            iframe.onclick = (e) => e.stopPropagation();
+
+                            modalContainer.appendChild(iframe);
+                            document.body.appendChild(modalContainer);
+                        } else {
+                            console.error("No text selected.");
+                        }
+                    }
+                });
+            } else {
+                console.error("No active tab found.");
+            }
+        });
+    }
+});
+
+function getSelectedTextAndShowModal() {
+    const selectedText = window.getSelection().toString();
+    if (selectedText) {
+        showModalOnPage(selectedText);
+    } else {
+        console.error("No text selected.");
+    }
+}
 
 function showModalOnPage(selectedText) {
     const existingModal = document.getElementById('font-styler-modal-container');
